477623f0aa089ba0e19b92d0f66b255c74ae754c
Tim <timwilburn8@gmail.com>
2018-04-02 14:53:22 -0400

    started php form experiment, loaded xamp on computer

---
+++ app/contact.php
@@ -0,0 +1,21 @@
+<?php
+  $name = $_POST['name'];
+  $visitor_email = $_POST['email'];
+  $message = $_POST['message'];
+  
+  $email_from = 'acumulus8@gmail.com';
+  $email_subject = 'New Form Submission';
+  $email_body = 'User Name: $name.\n'.
+                  'User Email: $visitor_email.\n'.
+                    'User Message: $message.\n'.
+                  
+  $to = 'timwilburn8@gmail.com';
+  
+  $headers = 'from: $email_from \r\n';
+  
+  $headers .= 'Reply-To: $visitor_email \r\n';
+  
+  mail($to,$email_subject,$email_body,$headers);
+  
+  header('Location: index.html');
+?>
