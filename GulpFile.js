6d995f648877791b1aab04e93ad5e03da2d792c9
Tim <timwilburn8@gmail.com>
2018-04-05 03:34:39 -0400

    added watch tasks, webpack, browser sync, thank you page on form submission

--- GulpFile.js
+++ GulpFile.js
@@ -1,2 +1,3 @@
 require('./gulp/tasks/watch');
 require('./gulp/tasks/sass');
+require('./gulp/tasks/scripts');
--- app/assets/scripts/App.js
+++ app/assets/scripts/App.js
@@ -1,3 +1,5 @@
 import ContactForm from './modules/ContactForm';
 
 let contactForm = new ContactForm();
+
+console.log('hey dawg');
