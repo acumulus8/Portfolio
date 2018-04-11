5ab81300a47bb67d68462ef4c751bd067f7cc77b
Tim <timwilburn8@gmail.com>
2018-04-11 00:57:14 -0400

    completed validation functionality and styles

--- app/assets/scripts/modules/FormValidate.js
+++ app/assets/scripts/modules/FormValidate.js
@@ -1,8 +1,27 @@
-import jquery from 'jquery';
+import $ from 'jquery';
 
 class FormValidate {
     constructor() {
+        this.form = $('#contact-form');
+        this.name = $('#name');
+        this.email = $('#email');
+        this.message = $('#message');
+        this.button = $('#button');
+        this.error = $('#error');
+        this.validateForm();
+    }
 
+    validateForm() {
+        const that = this;
+        this.form.keyup(function() {
+            if (that.email.val() !== '' && that.name.val() !== '' && that.message.val() !== '') {
+                that.button.addClass('button--contact-form--is-validated');
+                that.error.addClass('contact-form__error--is-hidden');
+            } else {
+                that.button.removeClass('button--contact-form--is-validated');
+                that.error.removeClass('contact-form__error--is-hidden');
+            }
+        });
     }
 };
 
