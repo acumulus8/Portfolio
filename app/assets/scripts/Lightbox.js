779bd41284e0cd1873b7b87752bad8a62c0077c3
Tim <timwilburn8@gmail.com>
2023-08-14 14:46:42 -0400

    feat: update certificates, improve lightbox layout styling

--- app/assets/scripts/Lightbox.js
+++
@@ -1,5 +0,0 @@
-import $ from 'jquery';
-import Lightbox from './modules/Lightbox';
-
-new Lightbox($('.js-lightbox'), $('.js-certs-btn'), $('.js-x-cert'), 'lightbox--is-visible');
-new Lightbox($('.js-resume'), $('.js-resume-btn'), $('.js-x-resume'), 'resume--is-visible');
