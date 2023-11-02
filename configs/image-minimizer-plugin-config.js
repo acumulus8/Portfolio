d3992e35f1fac5f5131124005af471363f5ca735
Tim <timwilburn8@gmail.com>
2023-11-02 17:12:16 -0400

    test: try new config for image minifier

--- configs/image-minimizer-plugin-config.js
+++ configs/image-minimizer-plugin-config.js
@@ -1,11 +1,17 @@
-module.exports = (ImageMinimizerPlugin) => {
+module.exports = (ImageMinimizerPlugin, isProduction) => {
 	return new ImageMinimizerPlugin({
 		minimizer: {
 			// Implementation
 			implementation: ImageMinimizerPlugin.imageminMinify,
 			// Options
 			options: {
-				plugins: ["imagemin-gifsicle", "imagemin-mozjpeg", "imagemin-pngquant", "imagemin-svgo"],
+				// plugins: ["imagemin-gifsicle", "imagemin-mozjpeg", "imagemin-pngquant", "imagemin-svgo"],
+				plugins: [
+					["imagemin-pngquant", { quality: [0.65, 0.9] }],
+					["imagemin-mozjpeg", { quality: 65, progressive: true }],
+					["imagemin-svgo"],
+					["imagemin-gifsicle"],
+				],
 			},
 		},
 	});
