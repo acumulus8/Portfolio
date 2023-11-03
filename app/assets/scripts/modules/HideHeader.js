8f183949530cada031c19d3f6d06f1026fd0b7ab
Tim <timwilburn8@gmail.com>
2023-11-02 23:54:16 -0400

    fix: reduce overall bundle size
    This includes removing some jquery and adjusting where necessary in the javascript to vanilla js. This is most definitely out of scope for a branch like this and should have been bundled in it's own branch :)

--- app/assets/scripts/modules/HideHeader.js
+++ app/assets/scripts/modules/HideHeader.js
@@ -1,49 +1,45 @@
-import $ from 'jquery';
-
 class HideHeader {
-    constructor() {
-        this.window = $(window);
-        this.siteHeader = $('.site-header');
-        this.triggerElement = $('#trigger');
-        this.toggleHeader();
-    }
+	constructor() {
+		this.window = window;
+		this.siteHeader = document.getElementsByClassName("site-header");
+		this.triggerElement = document.getElementById("trigger");
+		this.toggleHeader();
+	}
 
-    toggleHeader() {
-        const that = this;
-        let didScroll;
-        let lastScrollTop = 0;
-        let delta = 5;
-        let heroHeight = this.triggerElement.outerHeight();
+	toggleHeader() {
+		const that = this;
+		let didScroll;
+		let lastScrollTop = 0;
+		let delta = 5;
+		let heroHeight = this.triggerElement.outerHeight;
 
-        // on scroll, let the interval function know the user has scrolled
-        $(window).scroll(function(event){
-            didScroll = true;
-        });
-        // run hasScrolled() and reset didScroll status
-        setInterval(function() {
-            if (didScroll) {
-                hasScrolled();
-                didScroll = false;
-            }
-        }, 250);
-        function hasScrolled() {
-            let st = that.window.scrollTop();
-            if (Math.abs(lastScrollTop - st) <= delta)
-                return;
-                if (st > lastScrollTop && st > heroHeight){
-                    // Scroll Down
-                    that.siteHeader.addClass('site-header--hide');
-                } else {
-                    // Scroll Up
-                    if(st + $(window).height() < $(document).height()) {
-                        that.siteHeader.removeClass('site-header--hide');
-                    }
-                }
-                
-                lastScrollTop = st;
-            }
-            
-        } 
+		// on scroll, let the interval function know the user has scrolled
+		that.window.scroll(function (event) {
+			didScroll = true;
+		});
+		// run hasScrolled() and reset didScroll status
+		setInterval(function () {
+			if (didScroll) {
+				hasScrolled();
+				didScroll = false;
+			}
+		}, 250);
+		function hasScrolled() {
+			let st = that.window.scrollTop();
+			if (Math.abs(lastScrollTop - st) <= delta) return;
+			if (st > lastScrollTop && st > heroHeight) {
+				// Scroll Down
+				that.siteHeader.addClass("site-header--hide");
+			} else {
+				// Scroll Up
+				if (st + $(window).height() < $(document).height()) {
+					that.siteHeader.removeClass("site-header--hide");
+				}
+			}
+
+			lastScrollTop = st;
+		}
+	}
 }
 
 export default HideHeader;
--- app/assets/scripts/modules/ImageCarousel.js
+++ app/assets/scripts/modules/ImageCarousel.js
@@ -1,87 +1,83 @@
-import $ from 'jquery';
-
 class ImageCarousel {
-    constructor() {
-        this.slider = $('.slider');
-        this.slideContainer = $('#slideContainer');
-        this.img = $('.img');
-        this.imgWidth = $('.img').width();
-        this.currentSlide = 1;
-        this.slideNumber = 1;
-        this.pause = 3000;
-        this.animateTime = 800;
-        this.next = $('#next');
-        this.prev = $('#prev');
-        this.slideImage();
-        this.previousImage();
-        this.nextImage();
-    }
+	constructor() {
+		this.slider = document.getElementsByClassName("slider")[0];
+		this.slideContainer = document.getElementById("slideContainer");
+		this.img = document.getElementsByClassName("img")[0];
+		this.imgWidth = this.img.offsetWidth;
+		this.currentSlide = 1;
+		this.slideNumber = 1;
+		this.pause = 3000;
+		this.animateTime = 800;
+		this.next = document.getElementById("next");
+		this.prev = document.getElementById("prev");
+		this.slideImage();
+		this.previousImage();
+		this.nextImage();
+	}
 
-    slideImage() {
-        const that = this;
-        let interval;
+	slideImage() {
+		const that = this;
+		let interval;
 
-        function startSlider() {
-            interval = setInterval(function() {
-                that.slideContainer.animate({'margin-left': '-=' + that.imgWidth}, that.animateTime, function() {
-                    that.currentSlide++;
-                    that.slideNumber++;
-                    if (that.currentSlide == that.img.length -1) {
-                        that.currentSlide = 1;
-                        that.slideNumber = 1;
-                        that.slideContainer.css('margin-left', 0);
-                    }
-                });
-            }, that.pause);
-        }
+		function startSlider() {
+			interval = setInterval(function () {
+				that.slideContainer.animate({ "margin-left": "-=" + that.imgWidth }, that.animateTime, function () {
+					that.currentSlide++;
+					that.slideNumber++;
+					if (that.currentSlide == that.img.length - 1) {
+						that.currentSlide = 1;
+						that.slideNumber = 1;
+						that.slideContainer.css("margin-left", 0);
+					}
+				});
+			}, that.pause);
+		}
 
-        startSlider();
+		startSlider();
 
-        function stopSlider() {
-            clearInterval(interval);
-        }
+		function stopSlider() {
+			clearInterval(interval);
+		}
 
-        function mouseStuff() {
-            that.slider.mouseenter(function() {
-                stopSlider();
-            });
-            that.slider.mouseleave(function() {
-                startSlider();
-            })
-        }
+		function mouseStuff() {
+			that.slider.mouseenter(function () {
+				stopSlider();
+			});
+			that.slider.mouseleave(function () {
+				startSlider();
+			});
+		}
 
-        mouseStuff();
-    }
+		mouseStuff();
+	}
 
-    previousImage() {
-        const that = this;
-        this.prev.on('click', function() {
-            that.slideContainer.animate({'margin-left': '+=' + that.imgWidth}, that.animateTime,
-            function() {
-                that.currentSlide--;
-                that.slideNumber--;
-                if (that.currentSlide === 0) {
-                    that.currentSlide = that.img.length -2;
-                    that.slideContainer.css('margin-left', '-=' + that.imgWidth * 4);
-                }
-            });
-        });
-    }
+	previousImage() {
+		const that = this;
+		this.prev.on("click", function () {
+			that.slideContainer.animate({ "margin-left": "+=" + that.imgWidth }, that.animateTime, function () {
+				that.currentSlide--;
+				that.slideNumber--;
+				if (that.currentSlide === 0) {
+					that.currentSlide = that.img.length - 2;
+					that.slideContainer.css("margin-left", "-=" + that.imgWidth * 4);
+				}
+			});
+		});
+	}
 
-    nextImage() {
-        const that = this;
-        this.next.on('click', function() {
-            that.slideContainer.animate({'margin-left': '-=' + that.imgWidth}, that.animateTime,
-            function() {
-                that.currentSlide++;
-                that.slideNumber++;
-                if (that.currentSlide == that.img.length -1) {
-                    that.currentSlide = 1;
-                    that.slideContainer.css('margin-left', 0);
-                }
-            });
-        });
-    }
+	nextImage() {
+		const that = this;
+		this.next.on("click", function () {
+			that.slideContainer.animate({ "margin-left": "-=" + that.imgWidth }, that.animateTime, function () {
+				that.currentSlide++;
+				that.slideNumber++;
+				if (that.currentSlide == that.img.length - 1) {
+					that.currentSlide = 1;
+					that.slideContainer.css("margin-left", 0);
+				}
+			});
+		});
+	}
 }
 
 export default ImageCarousel;
